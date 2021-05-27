32ee45d5106425402a1d8b9fdb80b632f695a264
torresviagrazielle <201810909@gordoncollege.edu.ph>
2021-05-27 19:59:06 +0800

    Add Headers

--- resources/views/residents/archive.blade.php
+++ resources/views/residents/archive.blade.php
@@ -3,7 +3,15 @@
 @section('content')
 
 <div class="container">
-
+    <!--HEADER-->
+    <div class="form-inline">
+        <a class="btn button btn-light mb-3" style="font-size: 20px; font-weight:bold;" href="/residents">
+        <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="currentColor" class="bi bi-arrow-left-short" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M12 8a.5.5 0 0 1-.5.5H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5a.5.5 0 0 1 .5.5z"/></svg>  
+        </a>
+        <div class="form-group mb-2">
+            <h3 style="padding-left: 30px; font-weight:bold; color:#272a2d;">Resident Profiling Archive</h3>    
+        </div>
+    </div> <hr style="margin-top:-5px;"> 
 
     <div class="row justify-content-center">
         <div class="col-md-10">
